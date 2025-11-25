e70abc5e20aa06fe84913aebac7c5ea99267a7c6
CarmelaSuico <24101113@usc.edu.ph>
2025-11-25 23:33:58 +0800

    Add 'Add New Exercise' link to admin dashboard

--- mindfulness/admin/admindashboard.php
+++ mindfulness/admin/admindashboard.php
@@ -168,6 +168,7 @@ class AdminView {
     <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
             <li class="nav-item"><a class="nav-link" href="editusers.php">Edit Users</a></li>
+            <li class="nav-item"><a class="nav-link" href="addexercise.php">Add New Exercise</a></li>
             <li class="nav-item"><a class="nav-link" href="setting.php">Settings</a></li>
             <li class="nav-item"><a class="nav-link" href="../src/logout.php">Logout</a></li>
         </ul>
